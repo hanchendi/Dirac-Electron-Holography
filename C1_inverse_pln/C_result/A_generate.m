f017f0d1b80b700af8381699f97d3692070d36d4
Chendi Han <81707752+hanchendi@users.noreply.github.com>
2024-06-18 14:44:59 -0700

    Update A_generate.m

--- C1_inverse_pln/C_result/A_generate.m
+++ C1_inverse_pln/C_result/A_generate.m
@@ -6,6 +6,8 @@ rng(floor(mod(now*10^5,10^5)))
 phi=zeros(4,64,64);
 score_gather=0;
 
+%% The value for E and V is not normalized, which means E from 5 to 10 and V from -5 to 20
+
 E=10;
 V=[ 0.0826141   0.49327295  7.38519428  1.93001801 -0.47027141 12.48324335];
 [psiA,psiB,score,ee] = MMP_single(E,V);
